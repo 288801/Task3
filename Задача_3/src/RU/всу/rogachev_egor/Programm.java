6988c3ecc13271cc06a1b1beaa7810448136fd74
288801 <55750592+288801@users.noreply.github.com>
2021-09-28 11:59:11 +0300

    Update and rename Task_3/src/ru/vsu/rogachev_egor/Programm.java to Задача_3/src/RU/всу/rogachev_egor/Programm.java

--- Задача_3/src/RU/всу/rogachev_egor/Programm.java
+++ Задача_3/src/RU/всу/rogachev_egor/Programm.java
@@ -1,7 +1,7 @@
 package ru.vsu.rogachev_egor;
 
 public class Programm {
-    public static final Line L1 = new Line(-1, 8, 15.0 / 7);
+    public static final Line L1 = new Line(-1, 8, 15.0 / 6);
     public static final HorizontalParabola HP1 = new HorizontalParabola(1, 1, -1);
     public static final VerticalParabola VP1 = new VerticalParabola(0, 0, 1.0 / 8);
     public static final Rectangle R1 = new Rectangle(1, 5, -2, 0);
